a234a0ac402fa9cea77b2ad5120dd14a9c46f04b
Darlin2972 <darline6@nycstudents.net>
2025-10-28 01:12:26 +0000

    yikes

---
+++ Lesson-15-Activity/Main.java
@@ -0,0 +1,32 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+System.out.println("Done");
+}
+    
+  
+int spaces(int N){
+ int spaces = 5;
+for(int x=5; x<=N; x++)
+ spaces+=x;
+ return spaces;
+}
+int harmonic(int N){
+int harmonic = 1/1;
+for(int x=1/1; x>=1/5; x++)
+harmonic+=x;
+return harmonic;
+}
+
+int sine(int sin){
+  int sine
+}
+
+}
