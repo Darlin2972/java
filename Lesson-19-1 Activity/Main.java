087ef8e8dc3529119e3ee7eb8a0b1be0fc536808
Darlin2972 <darline6@nycstudents.net>
2025-11-19 13:50:28 +0000

    oofffff

---
+++ Lesson-19-1 Activity/Main.java
@@ -0,0 +1,32 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+
+  void init(){
+print(Math.random()*(6)+1);
+
+    
+  }
+  int rollDice(int Dice1, int Dice2){
+    Dice1= 6;
+    Dice2= 1;
+  (int)(Math.random()+(Dice1-Dice2+1))+1;
+
+String dice1 ="";
+String dice2 ="";
+return dice1,dice2;
+
+ 
+
+
+  }
+
+
+
+
+}
