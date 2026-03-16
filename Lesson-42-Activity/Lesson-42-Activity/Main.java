28f4916bc0cdd18825fce18ca1734e5ded84760b
Darlin2972 <darline6@nycstudents.net>
2026-03-16 13:34:55 +0000

    adwasdwa

---
+++ Lesson-42-Activity/Lesson-42-Activity/Main.java
@@ -0,0 +1,42 @@
+
+import java.io.IOException;
+import java.sql.*;
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+   
+    String queryResult ="";
+
+    Database db = new Database("jdbc:sqlite:students.db" );		
+    
+	    String sql = "Select * from cr101 Where studentId='STUDENT1001' and period=3 ;";	
+
+      queryResult = db.runSQL(sql,"table-auto");
+
+      print(queryResult);
+
+
+       sql ="Update cr101 ";
+       sql += "Set room=122 ";
+       sql += "Where studentId='STUDENT1001' and period=3;";
+
+    queryResult = db.runSQL(sql,"table-auto");
+      
+      print(queryResult);
+
+
+
+      }    
+}
