3b21c80458d7bd6f051aad31f02ad8017bd13dd3
Darlin2972 <darline6@nycstudents.net>
2026-05-20 13:34:10 +0000

    12313

--- pd3 project website/Lesson56DiscussionSolution/script.js
+++ pd3 project website/Lesson56DiscussionSolution/script.js
@@ -1,30 +1,25 @@
 
-// Data contain the hero name, image file name and description.
 let data,Games;
 
-/*
-Problem 
 
-Using the array of JSON called data above which includes the name, image file name and description to build a flip card for each hero.
-
-Have the front of the card display the image of the hero and the back of the card display the name as a header and a description below it.
-
-Add the card to the container "flipcard_output"
-*/
   async function init(){
-  let Games = "https://studious-space-yodel-4jv74vxqqqvg3q56g-8500.app.github.dev/Games";
+  let link = "https://studious-space-yodel-4jv74vxqqqvg3q56g-8500.app.github.dev/Games";
   let route= "/Games"
   info = await fetch(link+route);
   data = await info.json();
   console.log(data);
+  generatecards(data);
+  }
 
- window.onload = function(){ 
+ function generatecards(Games){ 
   let back="";
   let front="";
   
   for(let i=0; i<Games.length; i++){
     let Game = Games[i];
-    front = `<img src='images/${Game.img}'>`;
+    //front = `<img src='images/${data.img}'>`;
+    front = `<img src='images/${Game.img}.jpg'>`;
+
     back = `<h2>${Game.GameTitle}</h2>`;
     back+= `<p>${Game.Description}</p>`;
     back+= `<p>${Game.Genre}</p>`;
@@ -35,7 +30,5 @@ Add the card to the container "flipcard_output"
     card = new FlipCard(front,back);
     card.render("flipcard_output");
   }
-
- 
 }
-  }
+  
